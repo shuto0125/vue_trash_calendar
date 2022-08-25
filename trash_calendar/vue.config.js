0e93ac1c1c7c95ccb9c82002717e81d08a57a567
かいたにしゅうと <shuuto@kaitanishuutonoMacBook-Air.local>
2022-08-25 19:12:17 +0900

    productionファイル build 用のディレクトリ名修正

--- trash_calendar/vue.config.js
+++ trash_calendar/vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/trash_calendar/'
+    ? '/vue_trash_calendar/'
     : '/'
 })
